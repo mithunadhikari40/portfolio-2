7f77c0340c045fb35d01d6deea3d0a946aeb7e9d
Mithun Adhikari <mithunadhikari40@gamil.com>
2021-11-08 15:25:29 +0545

    footer is changed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,12 +12,12 @@ export default function Footer() {
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           {emoji("Made with ❤️ by Mithun Adhikari(Inspired from Saad Pasta) ")}
         </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+        {/* <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
           <a href="https://github.com/saadpasta/developerFolio">
             developerFolio
           </a>
-        </p>
+        </p> */}
       </div>
     </Fade>
   );
